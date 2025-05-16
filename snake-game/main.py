6a0fe60c1010e475f2411cde1b3d21127fe6bd71
Rojen Dangol <rojendangol44@gmail.com>
2025-05-16 08:47:09 +0545

    snake-finish

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title('My Snake Game')
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(key='Up', fun=snake.up)
@@ -22,7 +26,19 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
-    if snake.head.xcor() > 295:
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.score_count()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
