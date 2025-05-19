cd53de25514e953231e93ba334ddcaf63e9f2960
Rojen Dangol <rojendangol44@gmail.com>
2025-05-19 09:48:24 +0545

    exception-handling in NATO

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -4,7 +4,18 @@ data = pandas.read_csv('nato_phonetic_alphabet.csv')
 # print(data.to_dict())
 
 new_data = {row['letter']: row['code'] for (index, row) in data.iterrows()}
+print(new_data)
 
-user_input = input("Enter a word: ").upper()
-output_list = [new_data[name] for name in user_input]
-print(output_list)
+
+def fetch_word():
+    user_input = input("Enter a word: ").upper()
+    try:
+        output_list = [new_data[name] for name in user_input]
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+        fetch_word()
+    else:
+        print(output_list)
+
+
+fetch_word()
